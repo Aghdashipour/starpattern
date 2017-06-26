8b4b058ef18cbcd8b418b7dc1f8b4bcd9160b2bb
farhoud-aghdashipour <farhoudaghdashipour@gmail.com>
2017-06-27 02:51:22 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.write('<center>')
+var i, j;
+//outer loop
+  for(i=1; i <= 5; i++)
+   {
+//inner loop
+    for(j=1; j<=i; j++)
+   {
+     document.write('<img src="./images.png">');
+    }
+     document.write('<br/>');
+   }
